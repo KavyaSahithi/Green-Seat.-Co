89f8d57da227ef1ba2b5db1c3b4c6a500ee0bebb
Kavya Sahithi <kavya.lily@gmail.com>
2021-01-04 12:01:34 +0100

    login feature removed

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import { FaShoppingCart, FaUserMinus, FaUserPlus } from 'react-icons/fa'
+import { FaShoppingCart } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import { useProductsContext } from '../context/products_context'
 import { useCartContext } from '../context/cart_context'
-import { useUserContext } from '../context/user_context'
+
 const CartButton = () => {
   const { closeSidebar } = useProductsContext()
   const { total_items } = useCartContext()
-  const { loginWithRedirect, myUser, logout } = useUserContext()
+
   return (
     <Wrapper className='cart-btn-wrapper'>
       <Link to='/cart' className='cart-btn' onClick={closeSidebar}>
@@ -18,36 +18,13 @@ const CartButton = () => {
           <span className='cart-value'>{total_items}</span>
         </span>
       </Link>
-      {myUser ? (
-        <button
-          type='button'
-          className='auth-btn'
-          onClick={() => {
-            logout({ returnTo: window.location.origin })
-          }}
-        >
-          Logout <FaUserMinus />
-        </button>
-      ) : (
-        <button
-          type='button'
-          className='auth-btn'
-          onClick={() => {
-            console.log('In progress')
-          }}
-        >
-          Login <FaUserPlus />
-        </button>
-      )}
     </Wrapper>
   )
 }
 
 const Wrapper = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  align-items: center;
-  width: 225px;
+ 
+  padding:1rem;
 
   .cart-btn {
     color: var(--clr-grey-1);
@@ -55,7 +32,6 @@ const Wrapper = styled.div`
     letter-spacing: var(--spacing);
     color: var(--clr-grey-1);
     display: flex;
-
     align-items: center;
   }
   .cart-container {
@@ -82,18 +58,6 @@ const Wrapper = styled.div`
     color: var(--clr-white);
     padding: 12px;
   }
-  .auth-btn {
-    display: flex;
-    align-items: center;
-    background: transparent;
-    border-color: transparent;
-    font-size: 1.5rem;
-    cursor: pointer;
-    color: var(--clr-grey-1);
-    letter-spacing: var(--spacing);
-    svg {
-      margin-left: 5px;
-    }
   }
 `
 export default CartButton
